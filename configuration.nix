4ef2d7087956e733a82422574e1bf98e48b2b0e5
ianh <ianhuezo@gmail.com>
2024-09-13 11:59:49 -0400

    add mouse accel

--- configuration.nix
+++ configuration.nix
@@ -75,6 +75,8 @@
     # no need to redefine it in your config for now)
     #media-session.enable = true;
   };
+  services.libinput.mouse.accelPointsMotioon = 0.0;
+  services.libinput.mouse.accelPointsFallback = 0.0;
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
