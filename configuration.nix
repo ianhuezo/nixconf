87d6456ed5b99ce8b8eebd51e2a57a4f7c68a549
ianh <ianhuezo@gmail.com>
2024-09-13 01:11:30 -0400

    noisetorch functioning properly...

--- configuration.nix
+++ configuration.nix
@@ -117,8 +117,9 @@
      vesktop
      gnumake
      gcc
+     noisetorch
   ];
-
+  programs.noisetorch.enable = true;
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
