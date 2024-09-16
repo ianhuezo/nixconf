8efb3a93ec9234b316407f12b9a3711ff8461039
ianh <ianhuezo@gmail.com>
2024-09-16 18:48:17 -0400

    add some basic styling

--- home.nix
+++ home.nix
@@ -117,6 +117,14 @@ in
             ) 9
           )
         );
+      general = {
+        gaps_in = 5;
+        gaps_out = 20;
+        border_size = 2;
+        # "col.active_border" = "rgba(${config.colorScheme.palette.base08}ee) rgba(${config.colorScheme.palette.base0A}ee) 45deg";
+        # "col.inactive_border" = "rgba(${config.colorScheme.palette.base03}aa)";
+        layout = "master";
+      };
     };
   };
   wayland.windowManager.hyprland.systemd.variables = [ "--all" ];
