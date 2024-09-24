6a5287e21c647dd2a90035671df7231fd3e11892
ianh <ianhuezo@gmail.com>
2024-09-24 17:54:31 -0400

    attempting to get key bindings correct

--- home.nix
+++ home.nix
@@ -169,7 +169,6 @@ in
     ];
   };
 
-
   wayland.windowManager.hyprland = {
     enable = true;
     xwayland.enable = true;
@@ -230,13 +229,15 @@ in
               in
               [
                 "$mod, code:1${toString i}, workspace, ${toString ws}"
-                "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+                # " code:1${toString i}, movetoworkspace, ${toString ws}"
               ]
             ) 9
           )
         );
       #tokyodark theme applied
       general = {
+        resize_on_border = true;
+        hover_icon_on_border = true;
         gaps_in = 5;
         gaps_out = 5;
         border_size = 2;
@@ -246,6 +247,21 @@ in
       };
       decoration = {
         rounding = 10;
+      };
+      animations = {
+        enabled = "yes";
+
+        # Some default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
+
+        bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+        animation = [
+          "windows, 1, 7, myBezier"
+          "windowsOut, 1, 7, default, popin 80%"
+          "border, 1, 10, default"
+          "borderangle, 1, 8, default"
+          "fade, 1, 7, default"
+          "workspaces, 1, 6, default"
+        ];
       };
     };
   };
