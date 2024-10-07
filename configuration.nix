eeb94873a15636d85a9f6bfd25c2958de9a2fc57
ianh <ianhuezo@gmail.com>
2024-10-06 22:09:15 -0400

    add libnotify

--- configuration.nix
+++ configuration.nix
@@ -172,6 +172,7 @@
   environment.systemPackages = with pkgs; [
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
+    libnotify
     inotify-tools
     sysstat
     networkmanagerapplet
