52e8453fa59336f3e21be1b4da825004cbee5960
Ian Huezo <ianhuezo@gmail.com>
2025-01-12 14:23:18 -0500

    bar changes song title correctly

--- dotfiles/ags/app.tsx
+++ dotfiles/ags/app.tsx
@@ -4,54 +4,75 @@ import style from "style.scss"
 import Mpris from "gi://AstalMpris"
 import { astalify, type ConstructProps } from "astal/gtk4"
 
-function MusicWidget({ player }: { player: Mpris.Player }) {
-	const { START, END } = Gtk.Align
+function connectPlayerSignals(player: Mpris.Player, currentPlayer: Variable<Mpris.Player | undefined>, title: Variable<string>) {
+	// Connect to metadata changes
+	player.connect('notify::title', (player) => {
+		if (player.playbackStatus == Mpris.PlaybackStatus.PLAYING) {
+			title.set(player.title);
+		}
+	});
 
-	const title = bind(player, "title").as(t =>
-		t || "Unknown Track")
+	player.connect('notify::artist', (player) => {
+		if (player.playbackStatus == Mpris.PlaybackStatus.PLAYING) {
 
-	const artist = bind(player, "artist").as(a =>
-		a || "Unknown Artist")
+		}
+	});
 
-	const coverArt = bind(player, "coverArt").as(c =>
-		`background-image: url('${c}')`)
-
-	//const playerIcon = bind(player, "entry").as(e =>
-	//Astal.Icon.lookup_icon(e) ? e : "audio-x-generic-symbolic")
-
-	const position = bind(player, "position").as(p => player.length > 0
-		? p / player.length : 0)
-
-	const playIcon = bind(player, "playbackStatus").as(s =>
-		s === Mpris.PlaybackStatus.PLAYING
-			? "media-playback-pause-symbolic"
-			: "media-playback-start-symbolic"
-	)
-
-	return (
-		<box
-			css={coverArt.get()}
-		>
-			<label label={title.get()} />
-		</box>
-	)
-
+	// Connect to playback status changes
+	player.connect('notify::playback-status', (player) => {
+		if (player.playbackStatus == Mpris.PlaybackStatus.PLAYING) {
+			currentPlayer.set(player)
+		}
+	});
 }
 
+function disconnectPlayerSignals(player: Mpris.Player) {
+	if (player == undefined) return;
+	player.disconnect('notify::title', (player) => { });
+	player.disconnect('notify::artist', (player) => { });
+	player.disconnect('notify::playback-status', (player) => { });
+}
+
+function getCurrentPlayer({ players }: { players: Array<Mpris.Player> }) {
+	if (players.length == 0) return undefined;
+	const onlyPlayingPlayers = players.filter(player => {
+		return player.get_playback_status() == Mpris.PlaybackStatus.PLAYING
+	});
+	if (onlyPlayingPlayers.length == 0) return undefined;
+	const spotifyPlayer = onlyPlayingPlayers.filter(player => {
+		return player.get_bus_name().toLowerCase().includes("spotify")
+	})
+	if (spotifyPlayer.length > 0) return spotifyPlayer[0];
+	return onlyPlayingPlayers[0]
+}
 
 export default function Bar(gdkmonitor: Gdk.Monitor) {
 	const { TOP, LEFT, RIGHT } = Astal.WindowAnchor
 	const mpris = Mpris.Mpris.new()
-	const currentPlayer = Variable("")
-	const playerAddedListener = currentPlayer.observe(mpris, "player-added", () => {
-		print("player added")
-		return Mpris.Mpris.get_default().get_players()[0].title
-	})
-	const playerClosedListener = currentPlayer.observe(mpris, "player-closed", () => {
-		print("player removed")
-		return "closed :("
-	})
+	const currentPlayer: Variable<Mpris.Player | undefined> = Variable(undefined)
+	const allPlayers: Variable<Array<Mpris.Player>> = Variable(Mpris.Mpris.new().get_players())
+	const title: Variable<string> = Variable("")
+	mpris.connect('player-added', (mpris, busName) => {
+		allPlayers.get().forEach(player => disconnectPlayerSignals(player));
+		allPlayers.set(Mpris.Mpris.get_default().get_players())
+		allPlayers.get().forEach(player => connectPlayerSignals(player, currentPlayer, title))
+		currentPlayer.set(getCurrentPlayer({ players: allPlayers.get() }))
+		title.set(currentPlayer.get() ? currentPlayer.get().title : "")
+	});
 
+	mpris.connect('player-closed', (mpris, busName) => {
+		allPlayers.get().forEach(player => disconnectPlayerSignals(player));
+		allPlayers.set(Mpris.Mpris.get_default().get_players())
+		if (allPlayers.get().length > 0) {
+			currentPlayer.set(getCurrentPlayer({ players: allPlayers.get() }))
+		} else {
+			currentPlayer.set(undefined)
+			title.set("")
+		}
+	});
+	allPlayers.get().forEach(player => connectPlayerSignals(player, currentPlayer, title))
+	currentPlayer.set(getCurrentPlayer({ players: allPlayers.get() }))
+	title.set(currentPlayer.get() != undefined ? currentPlayer.get().title : "")
 
 
 	return <window
@@ -62,7 +83,12 @@ export default function Bar(gdkmonitor: Gdk.Monitor) {
 		anchor={TOP | LEFT | RIGHT}
 		application={App}>
 		<centerbox cssName="centerbox">
-			<label label={bind(currentPlayer).as((value) => `${value}`)} />
+			<label
+				label={bind(title).as((value) => {
+					if (value == undefined) return "";
+					return title.get()
+				})}
+			/>
 		</centerbox>
 	</window>
 }
--- home.nix
+++ home.nix
@@ -99,26 +99,6 @@ in
   #VR config files written in home manager
   xdg.configFile."openxr/1/active_runtime.json".source =
     "${pkgs.monado}/share/openxr/1/openxr_monado.json";
-  # xdg.configFile."openvr/openvrpaths.vrpath".text = ''
-  #   {
-  #     "config" :
-  #     [
-  #       "${config.xdg.dataHome}/Steam/config"
-  #     ],
-  #     "external_drivers" : null,
-  #     "jsonid" : "vrpathreg",
-  #     "log" :
-  #     [
-  #       "${config.xdg.dataHome}/Steam/logs"
-  #     ],
-  #     "runtime" :
-  #     [
-  #       "${pkgs.opencomposite}/lib/opencomposite"
-  #     ],
-  #     "version" : 1
-  #   }
-  # '';
-  #
   programs.wofi.enable = true;
   programs.wofi.settings = {
     width = 300;
