b9063f6fc80f1177bfd96b466571df225b3a796b
Ian Huezo <ianhuezo@gmail.com>
2025-03-08 20:53:13 -0500

    add shell scripts

--- dotfiles/scripts/yt_to_mp3/youtube_dl.sh
+++ dotfiles/scripts/yt_to_mp3/youtube_dl.sh
@@ -1,11 +1,3 @@
-#!/bin/bash
-#Example script that runs
-if [ "$#" -lt 2 ]; then
-    echo "Usage: $0 <resolution> <youtube-url>"
-    exit 1
-fi
-
 RESOLUTION=$1
 URL=$2
-
 python youtube_dl_preview.py $RESOLUTION $URL
