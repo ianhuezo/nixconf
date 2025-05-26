2de67e80ab15c9d74c11cc6babca974a631050a9
Ian Huezo <ianhuezo@gmail.com>
2025-05-26 15:18:37 -0400

    WIP2

--- nix/joyboy/configuration.nix
+++ nix/joyboy/configuration.nix
@@ -264,7 +264,7 @@
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
     cava
-    valgrind
+    # valgrind
     ripgrep
     libnotify
     inotify-tools
