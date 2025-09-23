1386f48f82f882ae59956123ef150289d267c687
Ian Huezo <ianhuezo@gmail.com>
2025-09-22 20:44:47 -0400

    script path works correctly now

--- dotfiles/scripts/ai_color_creator/generate_wallpaper.sh
+++ dotfiles/scripts/ai_color_creator/generate_wallpaper.sh
@@ -4,4 +4,4 @@ WALLPAPER_PATH=$1
 PROMPT_PATH=$2
 API_KEY=$3
 
-python ~/.config/custom_scripts/ai_color_creator/color_generator.py $WALLPAPER_PATH ~/.config/custom_scripts/ai_color_creator/prompt.md $API_KEY #--quiet
+python ~/.config/custom_scripts/ai_color_creator/color_generator.py $WALLPAPER_PATH ~/.config/custom_scripts/ai_color_creator/prompt.md $API_KEY --quiet
