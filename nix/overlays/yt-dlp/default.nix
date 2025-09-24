bca149c808f0ccff2b7c543229ca2cae9a33a471
Ian Huezo <ianhuezo@gmail.com>
2025-09-23 21:50:37 -0400

    update yt-dlp

--- nix/overlays/yt-dlp/default.nix
+++ nix/overlays/yt-dlp/default.nix
@@ -1,12 +1,12 @@
 final: prev: {
   python3Packages = prev.python3Packages // {
     yt-dlp = prev.python3Packages.yt-dlp.overrideAttrs (oldAttrs: rec {
-      version = "2025.09.05";
+      version = "2025.09.23";
       src = prev.fetchFromGitHub {
         owner = "yt-dlp";
         repo = "yt-dlp";
         rev = version;
-        hash = "sha256-9y6OUVm6hNTTi5FFmd9DHcmAMrvSmDD+4kDe00aMTDI=";
+        hash = "sha256-pqdR1JfiqvBs5vSKF7bBBKqq0DRAi3kXCN1zDvaW3nQ=";
       };
     });
   };
