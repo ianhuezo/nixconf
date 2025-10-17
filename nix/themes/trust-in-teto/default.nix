925c16e781aa1a2d74da45b032c36ff4ac8e1fee
Ian Huezo <ianhuezo@gmail.com>
2025-10-16 22:58:18 -0400

    change back to default wallpaper

---
+++ nix/themes/trust-in-teto/default.nix
@@ -0,0 +1,24 @@
+{
+    slug = "trust-in-teto";
+    name = "Trust In Teto";
+    author = "AI Assistant (inspired by uploaded wallpaper)";
+    theme = "dark";
+    palette = {
+      base00 = "#241E1D";
+      base01 = "#382D2B";
+      base02 = "#4D3C38";
+      base03 = "#6E5550";
+      base04 = "#A19B8B";
+      base05 = "#B8B19A";
+      base06 = "#D1C7AB";
+      base07 = "#EADDC3";
+      base08 = "#D14533";
+      base09 = "#E68A4D";
+      base0A = "#F0E5B9";
+      base0B = "#4DA39C";
+      base0C = "#6A85A6";
+      base0D = "#D64835";
+      base0E = "#C2598A";
+      base0F = "#8C261B";
+    };
+  }
