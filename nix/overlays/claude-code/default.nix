c48820c663b10646a5579b4da36e82be943ee741
Ian Huezo <ianhuezo@gmail.com>
2025-10-17 23:54:05 -0400

    add overlay for claude b/c for some reason was very old version

---
+++ nix/overlays/claude-code/default.nix
@@ -0,0 +1,12 @@
+self: super: {
+  claude-code = super.claude-code.overrideAttrs (oldAttrs: rec {
+    version = "2.0.17";
+
+    src = super.fetchzip {
+      url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
+      hash = "sha256-uQ+iosjjs6dUT8MtnFwubJqp85pBtBYkmIXTlRGufQw=";
+    };
+
+    npmDepsHash = "sha256-g03tfNJjx/KIUu14/mqranKR4G8mo9ig74iRgCdiJuE=";
+  });
+}
