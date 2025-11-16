4f15ccaa0155e7f9c7a28d07c0a52816cfe2812b
Ian Huezo <ianhuezo@gmail.com>
2025-11-15 23:48:19 -0500

    another color

---
+++ nix/themes/stargazer-nemophila/default.nix
@@ -0,0 +1,24 @@
+{
+    slug = "stargazer-nemophila";
+    name = "Stargazer Nemophila";
+    author = "AI Assistant";
+    theme = "dark";
+    palette = {
+      base00 = "#19182A";
+      base01 = "#222038";
+      base02 = "#393853";
+      base03 = "#514F71";
+      base04 = "#79769B";
+      base05 = "#B6B3D9";
+      base06 = "#D5D2F2";
+      base07 = "#E8E6FC";
+      base08 = "#F77D9B";
+      base09 = "#F29D7D";
+      base0A = "#EBC57A";
+      base0B = "#68AE9A";
+      base0C = "#54D5D8";
+      base0D = "#5775F7";
+      base0E = "#C77DF1";
+      base0F = "#D59F5A";
+    };
+  }
