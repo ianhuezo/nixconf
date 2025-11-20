d7ff98278b4720daf54b254af2015a88ca17ac8b
Ian Huezo <ianhuezo@gmail.com>
2025-11-19 22:15:45 -0500

    another theme

---
+++ nix/themes/frieren-midnight-garden/default.nix
@@ -0,0 +1,24 @@
+{
+    slug = "frieren-midnight-garden";
+    name = "Midnight Garden";
+    author = "AI Assistant";
+    variant = "dark";
+    palette = {
+      base00 = "#0f1419";
+      base01 = "#1a1f2e";
+      base02 = "#252d42";
+      base03 = "#3d4762";
+      base04 = "#6b7894";
+      base05 = "#b4bdd6";
+      base06 = "#d0d6e8";
+      base07 = "#e8ecf5";
+      base08 = "#8a7fc4";
+      base09 = "#9d90d6";
+      base0A = "#7a9bd8";
+      base0B = "#6ba8c4";
+      base0C = "#74c7d9";
+      base0D = "#5b9dd6";
+      base0E = "#9a8fd6";
+      base0F = "#6f83b8";
+    };
+  }
